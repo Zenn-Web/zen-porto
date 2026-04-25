388123313e10bd045dfe97e165ab76f28f1b6fa9
Zenn-Web <zenifenagusti70@gmail.com>
2026-04-25 23:22:54 +0700

    refinement v8 : refining ornamen di section hero, favicon

--- resources/views/layout/welcome.blade.php
+++ resources/views/layout/welcome.blade.php
@@ -4,6 +4,12 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Zenifen Agusti</title>
+
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
     
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,600;1,600&family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
     
